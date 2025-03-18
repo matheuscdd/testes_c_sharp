11d6e6a3c95e152ddc5419be74a3de1b0d7da653
matheuscdd <matheuscdd@hotmail.com>
2025-03-17 22:21:49 -0300

    feat: init models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Xml;
+
+namespace api.Models 
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        
+        // Navigation property serve para ajudar nas queries 
+        public Stock? Stock { get; set; } 
+    }
+}
